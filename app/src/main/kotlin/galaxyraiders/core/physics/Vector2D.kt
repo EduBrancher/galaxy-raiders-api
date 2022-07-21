3be6588375bad20f03105552f1676ae1c46e244e
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-07-21 20:51:23 -0300

    copilot made it for me =P

--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -14,10 +14,10 @@ data class Vector2D(val dx: Double, val dy: Double) {
     get() = sqrt(this.dx * this.dx + this.dy * this.dy)
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() = acos(this.dx / this.magnitude)
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() = atan2(this.dy, this.dx)
 
   val unit: Vector2D
     get() = Vector2D(this.dx / this.magnitude, this.dy / this.magnitude)
@@ -34,23 +34,23 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return this.dx * v.dx + this.dy * v.dy
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.dx + v.dx, this.dy + v.dy)
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(this.dx + p.x, this.dy + p.y)
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(-this.dx, -this.dy)
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.dx - v.dx, this.dy - v.dy)
   }
 
   fun scalarProject(target: Vector2D): Double {
