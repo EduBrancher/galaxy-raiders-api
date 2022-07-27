d2d9b9ac31ef24fcaa175cfc5981baa1647bb0f9
EduBrancher <edu.urenha93@gmail.com>
2022-07-26 21:54:04 -0300

    refiz o hack

--- app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
@@ -125,11 +125,13 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   }
 
   private fun defineMissilePosition(missileRadius: Double): Point2D {
-    return ship.center + Vector2D(dx = 0.0, dy = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip)
+    return ship.center + Vector2D(
+      dx = ship.radius + missileRadius + SpaceFieldConfig.missileDistanceFromShip,
+      dy = 0.0)
   }
 
   private fun defineMissileVelocity(): Vector2D {
-    return Vector2D(dx = 0.0, dy = 1.0)
+    return Vector2D(dx = 1.0, dy = 0.0)
   }
 
   private fun createAsteroidWithRandomProperties(): Asteroid {
